60c1ce8e2e135e62d774985cb5de4b302e20c2c3
dingyongya <yahya_dyy@163.com>
2018-01-08 11:22:53 +0800

    genggai

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,24 @@
 var gulp = require('gulp'),
-connect = require('gulp-connect');
+  connect = require('gulp-connect');
 
-var serveConfig ={
-    root:'./',
-    port:8081,
-    livereload:true
+var serveConfig = {
+  root: './',
+  host: '192.168.20.51',
+  port: 8081,
+  livereload: true
 }
 
-gulp.task('watch',function () {
-    gulp.watch('./*.html',['html']);
+gulp.task('watch', function () {
+  gulp.watch('./*.html', ['html']);
 })
 
-gulp.task('connect',function () {
-    connect.server(serveConfig);
+gulp.task('connect', function () {
+  connect.server(serveConfig);
 })
 
-gulp.task('html',function () {
-    gulp.src('./*.html')
-        .pipe(connect.reload());
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
 })
 
-gulp.task('default',['connect','watch']);
+gulp.task('default', ['connect', 'watch']);
